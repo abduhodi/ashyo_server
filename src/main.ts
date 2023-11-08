841f49a6d7c6c4722c3e6edc1bb6fb1dc6371100
abduhodi <tursunboyev.abduhodi1997@mail.ru>
2023-11-08 15:40:37 +0500

    main setting

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+async function start() {
   const port = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
   await app.listen(port, () => console.log('server listening on port ' + port));
 }
-bootstrap();
+start();
+//start
