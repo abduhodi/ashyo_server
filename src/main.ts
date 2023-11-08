631e422a821fb75bfd772af3c1d820f546a70f4e
abduhodi <tursunboyev.abduhodi1997@mail.ru>
2023-11-08 16:28:21 +0500

    port 3030

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function start() {
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 3030;
   const app = await NestFactory.create(AppModule);
   await app.listen(port, () => console.log('server listening on port ' + port));
 }
